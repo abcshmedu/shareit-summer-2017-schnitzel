bb294b33022245ec3c32edfff701ba92e025d547
truenicfel <dassler@hm.edu>
2017-05-27 14:31:27 +0200

    MediaRequest is now able to ask authorization server if the provided token in the request URI is valid

--- src/main/java/edu/hm/cs/schnitzel/dataExchange/MediaRequest.java
+++ src/main/java/edu/hm/cs/schnitzel/dataExchange/MediaRequest.java
@@ -7,14 +7,26 @@
  */
 package edu.hm.cs.schnitzel.dataExchange;
 
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.hm.cs.schnitzel.entities.Book;
 import edu.hm.cs.schnitzel.entities.Disc;
 import edu.hm.cs.schnitzel.services.MediaService;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Collections;
+import java.util.Optional;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.json.JSONObject;
 
 /**
  *
@@ -155,35 +167,145 @@ public class MediaRequest implements Request {
         return result;
     }
 
+    /**
+     * This checks if the user is authorized. There are three ways how the
+     * authorization can fail: 1. There is no token. 2. There is a token but its
+     * not valid. 3. The authorization service is offline
+     *
+     * @return If the user is not authorized this will return an appropriate
+     * result.
+     */
+    private Optional<Result> checkAuthorization() {
+        final Optional<Result> result;
+        final String requestURI = getRequest().getRequestURI();
+        //first check if token is there
+        if (requestURI.matches("[a-zA-Z/]*?token=[a-z]*")) {
+            //token
+            final String token
+                    = requestURI.substring(requestURI.indexOf("?") + 1);
+            //json string from token
+            final String json = "{token:" + token + "}";
+            //send request to auth service
+            //Optional will be empty if everything is alright
+            //2. There is a token but its not valid.
+            //3. The authorization server is offline.
+            result = sendRequestToAuthService(json);
+
+        } else {
+            //1. There is no token:
+            //TODO: URL des Auth Servers einfuegen
+            result = Optional.of(new MediaResult(HttpServletResponse.SC_BAD_REQUEST, "U didnt "
+                    + "provide a token with your request. Please provide the "
+                    + "token which you requested from the authorization server"
+                    + " (HIER URL EINFUEGEN) like this: "
+                    + ".../shareit/...?token=YOUR TOKEN HERE.",
+                    Collections.EMPTY_LIST));
+        }
+        return result;
+    }
+
+    /**
+     * This method will send a request to auth service to check the token.
+     *
+     * @param json The json which will be sent with the request.
+     * @return Empty optional if the token is valid. Else the result wrapped in
+     * an Optional.
+     */
+    private Optional<Result> sendRequestToAuthService(final String json) {
+        Optional<Result> result = Optional.empty();
+        try {
+            //open connection to auth service
+            //TODO: insert correct URL here
+            final URL url = new URL("insert correct URL here");
+            final HttpURLConnection httpURLConnection
+                    = (HttpURLConnection) url.openConnection();
+            //make it a output connection
+            httpURLConnection.setDoOutput(true);
+            //set method to post
+            httpURLConnection.setRequestMethod("POST");
+            //set own properties (content type = json + content length =
+            //json string length)
+            httpURLConnection.setRequestProperty("Content-Type",
+                    "application/json");
+            httpURLConnection.setRequestProperty("Content-Length",
+                    String.valueOf(json.length()));
+            //the answer from the auth server
+            final String answer;
+            //write data (use try to automatically close resources)
+            try (final OutputStream outputStream
+                    = httpURLConnection.getOutputStream();
+                    final BufferedReader bufferedReader
+                    = new BufferedReader(new InputStreamReader(
+                            httpURLConnection.getInputStream()))) {
+                //write data
+                outputStream.write(json.getBytes());
+                answer = bufferedReader.lines().collect(Collectors.joining());
+            }
+            //search for valid: true,
+            final Pattern pattern = Pattern.compile("valid: true,");
+            //if this  is true we have a valid token
+            if (!pattern.matcher(answer).find()) {
+                result = Optional.of(new MediaResult(
+                        HttpServletResponse.SC_UNAUTHORIZED,
+                        "Your token was not accepted.",
+                        Collections.EMPTY_LIST));
+            }
+        } catch (MalformedURLException ex) {
+            result = Optional.of(new MediaResult(
+                    HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Dont panic."
+                    + "Its not your fault.", Collections.EMPTY_LIST));
+            System.out.println("The URL was malformed.");
+            System.out.println(ex.toString());
+
+        } catch (IOException ex) {
+            result = Optional.of(new MediaResult(
+                    HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Dont panic."
+                    + "Its not your fault.", Collections.EMPTY_LIST));
+            System.out.println("An error occured while sending data to the auth"
+                    + "service.");
+            System.out.println(ex.toString());
+        }
+        //result will be empty if everything went alright
+        return result;
+    }
+
     //Methods Public
     //--------------------------------------------------------------------------
     @Override
     public final Result processRequest() {
         //the result which will be returned
         Result result;
-        //book or disc request
-        final String requestedResource = getRequest()
-                .getRequestURI().split("/")[INDEX_RESOURCE_TYPE];
-        try {
-            if ("books".equals(requestedResource)) {
-                result = delegateBookAction();
-            } else if ("discs".equals(requestedResource)) {
-                result = delegateDiscAction();
-            } else {
-                result = new MediaResult(HttpServletResponse.SC_NOT_FOUND,
-                        "Not found. The requested resource does not exist."
-                        + "Make sure to use the correct URI pattern."
-                        + "The pattern is as follows:"
-                        + "/shareit/media/books or discs/{isbn or barcode]",
+        //check if requested person is authorized first
+        final Optional<Result> optionalResult = checkAuthorization();
+        //present means user is authorized
+        if (optionalResult.isPresent()) {
+
+            //book or disc request
+            final String requestedResource = getRequest()
+                    .getRequestURI().split("/")[INDEX_RESOURCE_TYPE];
+            try {
+                if ("books".equals(requestedResource)) {
+                    result = delegateBookAction();
+                } else if ("discs".equals(requestedResource)) {
+                    result = delegateDiscAction();
+                } else {
+                    result = new MediaResult(HttpServletResponse.SC_NOT_FOUND,
+                            "Not found. The requested resource does not exist."
+                            + "Make sure to use the correct URI pattern."
+                            + "The pattern is as follows:"
+                            + "/shareit/media/books or discs/{isbn or barcode]",
+                            Collections.emptyList());
+                }
+            } catch (IOException exception) {
+                result = new MediaResult(
+                        HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+                        "A server error occured. This is not your fault. "
+                        + "You can calm down again. Info fuer den Chefinformatiker:"
+                        + "Ein Fehler is beim parsen des Requests aufgetreten.",
                         Collections.emptyList());
             }
-        } catch (IOException exception) {
-            result = new MediaResult(
-                    HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
-                    "A server error occured. This is not your fault. "
-                    + "You can calm down again. Info fuer den Chefinformatiker:"
-                    + "Ein Fehler is beim parsen des Requests aufgetreten.",
-                    Collections.emptyList());
+        } else {
+            result = optionalResult.get();
         }
         return result;
     }
